2ef1a61d643475e85e4860e626f081ee63b9aed5
Oric <oricricardo@gmail.com>
2023-02-07 18:59:05 -0300

    finalizando a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+250.times do
+    Car.create(
+        nome: Faker::Vehicle.make_and_model,
+        ano: Faker::Vehicle.year,
+        cor: Faker::Vehicle.color,
+        desc: Faker::Vehicle.car_options,
+        price: Faker::Number.decimal(l_digits: 3, r_digits: 3),
+        km: Faker::Vehicle.mileage,
+    )
+end
+Car.all.each do |car|
+    puts car.nome
+end
+Car.all.each do |car|
+    puts  "nome do carro: #{car.nome}"
+    puts  "ano do carro: #{car.ano}"
+    puts  "cor do carro #{car.cor}:"
+    puts  "desc do carro: #{car.desc}"
+    puts  "preco do carro: #{car.price}"
+end
